1cc2b2b146fe7ccd6ededc7affb66a5b01b71cf7
wastonluo <wastonluo@tencent.com>
2022-06-28 22:56:36 +0800

    add problem 605

---
+++ 605_can-place-flowers/can_place_flowers.py
@@ -0,0 +1,35 @@
+from typing import List
+from unittest import skip
+
+class Solution:
+    """
+    https://leetcode.cn/problems/can-place-flowers/
+    """
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        length = len(flowerbed)
+        skip = False
+        for i in range(length):
+            if skip:
+                skip = False
+                continue
+            current_ok = flowerbed[i] != 1
+            left_ok = flowerbed[i - 1] != 1 if i > 0 else True
+            right_ok = flowerbed[i + 1] != 1 if i < length - 1 else True
+            if current_ok and left_ok and right_ok:
+                flowerbed[i] = 1
+                n -= 1
+                skip = True
+
+            if n == 0:
+                break
+
+        return n == 0
+
+
+if __name__ == "__main__":
+    flowerbed = [1,0,0,0,1]
+    n = 1
+    s = Solution()
+    print(s.canPlaceFlowers(flowerbed, n))
